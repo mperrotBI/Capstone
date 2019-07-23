05bc2aff66a9cad44f9a58820c05acbb54f53c6d
Matthew Perrot <39060146+mperrotBI@users.noreply.github.com>
2019-07-23 18:46:04 -0230

    Add files via upload

--- EventSpace/js/app.js
+++ EventSpace/js/app.js
@@ -1,8 +1,7 @@
-
 var title = document.getElementById("title");
 var desc = document.getElementById("desc");
 var price = document.getElementById("price");
-var location = document.getElementById("location");
+var lo = document.getElementById("location");
 var fire = document.getElementById("fire");
 
 var submitBtn = document.getElementById("submitBtn");
@@ -10,10 +9,6 @@ var fireHeading = document.getElementById("fireHeading");
 
 var firebaseHeadingRef = firebase.database().ref().child("Heading");
 
-/*firebaseHeadingRef.on('value', function(datasnapshot){
-  fireHeading.innerText = datasnapshot.val();
-});
-*/
 var firebase = require('firebase/app');
 require('firebase/auth');
 require('firebase/database');
@@ -23,25 +18,11 @@ function submitClick() {
 
    var messageText = title.value;
 
-   firebaseRef.push().set(messageText);
+   firebaseRef.push().set({
+   Title: title.value,
+   Description: desc.value,
+   Price: price.value,
+   Location: lo.value,
+   FireCode: fire.value
+   });
 }
-
-/*
-$(document).ready(function(){
-
-
-  var rootRef = firebase.database().ref().child("Users");
-
-  rootRef.on("child_added", snap => {
-
-    var name = snap.child("Name").val();
-    var email = snap.child("Email").val();
-
-    $("#table_body").append("<tr><td>" + name + "</td><td>" +
-    email + "</td><td><button>Remove</button></td></tr>");
-  });
-})
-var firebase = require('firebase/app');
-require('firebase/auth');
-require('firebase/database');
-*/
